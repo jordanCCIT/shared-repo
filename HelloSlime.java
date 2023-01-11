a9e3db55fff851934f5279d095575dfdfe909da2
jordanCCIT <jordanadposting@gmail.com>
2023-01-10 19:38:46 -0500

    BadCommit

--- HelloSlime.java
+++ HelloSlime.java
@@ -2,6 +2,7 @@ public class HelloSlime{
     public static void main(String args[]) {
         System.out.println("Hello Slime!");
         System.out.println("This line was added in a branch!!!!!!!!");
+        System.out.println("This should cause a merge conflict!!");
     }
 
 }

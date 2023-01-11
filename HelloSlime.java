8d4d770b5754fcbb829c3fc0ed377cfc987044d9
jordanCCIT <jordanadposting@gmail.com>
2023-01-10 19:45:32 -0500

    new branch push

--- HelloSlime.java
+++ HelloSlime.java
@@ -2,7 +2,6 @@ public class HelloSlime{
     public static void main(String args[]) {
         System.out.println("Hello Slime!");
         System.out.println("This line was added in a branch!!!!!!!!");
-        System.out.println("This should cause a merge conflict!!");
         System.out.println("This will not be a merge conflict!!!!!");
     }
 
